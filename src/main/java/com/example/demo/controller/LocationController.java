1440baec6745ce73a38867e738255fe02f7a2198
bala-amypo <bala@amypo.in>
2025-12-15 17:43:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,6 @@ public class LocationController {
 
   @GetMapping("/locations")
      public List<Location> getAll(){
-        return lcs.get
+        return lcs.getAllLocations();
      }
 }
