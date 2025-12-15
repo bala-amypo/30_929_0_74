9e6b21168e6e29d2194c44883d438253fdf03757
bala-amypo <bala@amypo.in>
2025-12-15 17:13:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocationController {
-
+@PostMapping("/locations")
+  public Location addLocation(@RequestBody )
 }
