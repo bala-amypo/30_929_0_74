da11e70342231e48f9618be41e192bd2b0598de1
bala-amypo <bala@amypo.in>
2025-12-16 13:29:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -14,3 +14,37 @@ import com.example.demo.service.StudentService;
 @RestController
 public class StudentController {
 @Autowired
+StudentService studentService;
+@PostMapping("/PostStudent")
+public Student postStd(@RequestBody Student st){
+return studentService.insertStudent(st);
+}
+@GetMapping("/getAll")
+public List<Student> getAll(){
+return studentService.getAllStudents();
+}
+@GetMapping("/get/{id}")
+public Optional<Student> get(@PathVariable Long id){
+return studentService.getOneStudent(id);
+}
+@PutMapping("/update/{id}")
+public String update(@PathVariable Long id,@RequestBody Student
+newStudent){
+Optional<Student> student=studentService.getOneStudent(id);
+if(student.isPresent()){
+newStudent.setId(id);
+studentService.insertStudent(newStudent);
+return "Updated Success";
+}
+return "Id not found";
+}
+@DeleteMapping("/del/{id}")
+public String deleteStudent(@PathVariable Long id){
+Optional<Student> student=studentService.getOneStudent(id);
+if(student.isPresent()){
+studentService.deleteStudent(id);
+return "Deleted Success";
+}
+return "Id Not Found";
+}
+}
